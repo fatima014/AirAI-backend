9b24f1be380ce9e224ef87d5f157078aad4c8aab
Fatima <89465115+fatima014@users.noreply.github.com>
2024-05-01 11:36:03 +0500

    set up mysql connection pool

---
+++ pool.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql');
+
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  database: 'AirAI'
+});
+
+module.exports = pool;
